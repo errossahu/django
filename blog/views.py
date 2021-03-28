6583947c5d686c90b993cafe243ad6a440b0d81d
Yohanes Erros Sahu <duragonprojek@gmail.com>
2021-03-28 15:46:05 +0800

    Commit

--- blog/views.py
+++ blog/views.py
@@ -3,10 +3,22 @@ from django.shortcuts import render
 posts = [
     {
         'author':'Yohanes Erros Sahu',
-    }
+        'title':'Blog Post 1' , 
+        'content': 'First post content', 
+        'date_posted': 'Augest 25 2021', 
+    },
+    {
+        'author':'Jhon Doe 2',
+        'title':'Blog Post 1' , 
+        'content': 'First post content', 
+        'date_posted': 'Augest 25 2021', 
+    },
 ]
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'post': posts,  
+    }
+    return render(request, 'blog/home.html', context)
 def about(request):
     return render(request, 'blog/about.html')   
 
