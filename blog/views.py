30efeb13e1b2abe6d2ba94507c20bf4deb872497
Yohanes Erros Sahu <duragonprojek@gmail.com>
2021-03-28 15:11:26 +0800

    Frist COMMIT

---
+++ blog/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse 
+
+def home(request):
+    return HttpResponse('<h1>Blog Name</h1>')
+def about(request):
+    return HttpResponse('<h1>About</h1>')    
